c19bbbedfbfed387fcfc72aabfa9db584975c218
Palmese Davide Mattia <palmesedavidemattia@gmail.com>
2022-03-28 15:09:57 +0200

    fixed infinite render of CapsuleCard components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CapsuleCard from "./components/CapsuleCard";
 
 export default function App(){
 
-  let [capsules, setCapsules] = React.useState([])
+  let [capsules, setCapsules] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.spacexdata.com/v4/capsules')
+      .then(response =>  setCapsules(response.data))
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+    });
+  }, []);
 
 
-  axios.get('https://api.spacexdata.com/v4/capsules')
-  .then(function (response) {
-    setCapsules(response.data);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
 
   return(
     <div className="card--container">
--- src/components/CapsuleCard.js
+++ src/components/CapsuleCard.js
@@ -1,13 +1,14 @@
 import React from "react"
 
 export default function CapsuleCard(props){
+  console.log("rendered")
     return (
         <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{props.serial}</h5>
-          <p className="card-text">{props.last_update}</p>
-          
-        </div>
+          <div className="card-body">
+            <h5 className="card-title">{props.serial}</h5>
+            <p className="card-text">{props.last_update}</p>
+            
+          </div>
         </div>
     )
 }
